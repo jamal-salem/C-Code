6a33548540485254b222e98dcde5383a39291884
Jamal Salem <jaudi990@yahoo.com>
2026-06-06 14:40:36 +0400

    Add basic arithmetic and math functions example
    This program demonstrates basic arithmetic operations and mathematical functions in C.

---
+++ working with numbers.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+
+    printf("%d\n",5 + 4);
+
+
+    printf("%f\n", 5 + 4.5);
+
+
+    printf("%d\n",5 / 4);
+
+
+    printf("%f\n",5 / 4.0);
+
+    int num = 6;
+    printf("%d\n",num);
+
+
+    printf("%f\n", pow(4, 3) );
+
+    printf("%f\n", sqrt(36) );
+
+
+    printf("%f\n", ceil(36.356) );
+
+    printf("%f\n", floor(36.656) );
+
+
+
+
+
+
+
+    return 0;
+}
+
+
