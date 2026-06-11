0455d2b2fdb1dea23ef668cba9d5111f99a0f625
Jamal Salem <jaudi990@yahoo.com>
2026-06-12 02:25:40 +0400

    Add user input for age and grade

---
+++ Getting User Inpute.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("you are %d years old", age);
+
+
+    return 0;
+}
+
+
+
+----------------------------------------
+
+
+  #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+
+    char grade;
+    printf("Enter your grade: ");
+    scanf("%c", &grade);
+    printf("you grade is %c", grade);
+
+
+    return 0;
+}
+
